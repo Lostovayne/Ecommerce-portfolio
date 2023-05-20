ea905030aaf5da80ce78bda3569d26a08a67fb85
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-05-20 14:39:31 -0400

    scroll corregido

--- src/components/TextosBusqueda.jsx
+++ src/components/TextosBusqueda.jsx
@@ -9,7 +9,7 @@ const TextosBusqueda = () => {
       <h2 className="text-2xl font-medium text-white pl-4  lg:pl-0 ">
         Available Products
       </h2>
-      <section className=" pl-4 py-2  flex gap-4 overflow-x-auto justify-between overscroll-x-none w-10/12 lg:pl-0 lg:mb-2 ">
+      <section id="borrarScroll" className=" pl-4 py-2  flex gap-4 overflow-x-auto justify-between overscroll-x-contain w-full lg:pl-0 lg:mb-2 ">
         <ButtonFilters>All</ButtonFilters>
         <ButtonFilters>Bags</ButtonFilters>
         <ButtonFilters>Food</ButtonFilters>
